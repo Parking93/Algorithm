a8c83f3a7a45f6054847dfa8559c6aa485bbea7a
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-08-25 00:16:53 +0900

    Update 파이썬100제.py

--- 파이썬100제/파이썬100제.py
+++ 파이썬100제/파이썬100제.py
@@ -202,51 +202,35 @@ n = '10 9 8 7 6 5 4 3 2 1'
 max(map(int,n.split(' ')))
 
 
-"""# 53번
-괄호 문자열이란 괄호 기호인 '{', '}', '[', ']', '(', ')' 와 같은 것을 말한다. 그 중 괄호의 모양이 바르게 구성 된 문자열을 바른 문자열, 그렇지 않은 문자열을 바르지 않은 문자열이라 부르도록 하자.
-
-(())와 같은 문자열은 바른 문자열이지만 ()()) 와 같은 문자열은 바르지 않은 문자열이다.(해당 문제에서는 소괄호만 판별하지만, 실력이 되시는 분들은 중괄호와 대괄호까지 판별해보세요.)
-
-입력으로 주어진 괄호 문자열이 바른 문자열인지 바르지 않은 문자열인지 "YES"와 "NO"로 구분된 문자열을 출력해보자.
-"""
-
-s1 = '()())'
-s2 = '(0)'
-
-answer = ''
-
-for i in s:
-    if i in '(':
-        answer += i
-    elif i in ')':
-        if not answer or answer[-1] != ')':
-            print('No')
+# 53번
+def sol(s):
+    a = ''
+    for i in s:
+        if i in ('(', ')'):
+            a += i
+    b = ''
+    for i in a:
+        if i in '(':
+            b += i
+        elif (i in ')') and ('(' in b):
+            b = b[:-1]
+        else:
+            return('NO')
             break
+    if b == '':
+        return('YES')
+sol('(()))')
 
-"""# 54번
-
-"""
-
-s1 = '1 2 3 4 5'
-s2 = '1 4 2 6 3'
-def solution(s):
-    a = sorted(list(map(int, s.split(' '))))
-    if a == list(range(min(a), max(a)+1)):
-        return 'YES'
+# 54번
+def sol(num):
+    num = list(map(int, num.split(' ')))
+    if sorted(num) == list(range(min(num), max(num)+1)):
+        return('YES')
     else:
-        return 'NO'
+        return('NO')
+sol('1 4 2 6 3')
 
-solution(s1)
-
-"""# 55번
-
-재귀로 풉니다.
-
-한쪽 기둥에서 다른 마지막 기둥으로 모두 옮기는 것이 중요합니다.
-"""
-
-
-
+# 55번 
 # [[A, C], [A, B] c,b a,c b,a b,c a,c]]
 원판의이동경로 = []
 def 하노이(원반의수, 시작기둥, 목표기둥, 보조기둥):
@@ -266,12 +250,8 @@ def 하노이(원반의수, 시작기둥, 목표기둥, 보조기둥):
 print(len(원판의이동경로))
 원판의이동경로
 
-"""# 56번
+# 56번
 
-다음의 딕셔너리가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
-
-
-```
 데이터
 nationWidth = {
      'korea': 220877,
