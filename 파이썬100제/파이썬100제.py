b8ed240bab59272fbc22486dd69e91773477ce7d
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-08-28 23:03:45 +0900

    Update 파이썬100제.py

--- 파이썬100제/파이썬100제.py
+++ 파이썬100제/파이썬100제.py
@@ -299,42 +299,12 @@ str(list(range(0, 1001))).count('1')
 format(123456789, ',')
 
 # 59번
-
-
-"""
-
-userinput = input()
-print("{0:=^50}".format(user_input))
-
-userinput = 'hi'
-print(f'{userinput:=<50}')
-
-print(f'{userinput:=>50}')
-
 print(f'{userinput:=^50}')
 
-"""# 60번
+# 60번
 
-새학기가 되어 이름을 가나다 순서대로 배정하고 번호를 매기려고 합니다.
+# 61번
 
-코드에 입력된 이름을 아래와 같이 출력해주세요.
-"""
-
-student = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리','한지호','김진이','김민호','강채연']
-
-for i, j in enumerate(sorted(student)):
-    print(f'번호: {i+1}, 이름: {j}')
-
-"""# 61번
-문자열을 입력받고 연속되는 문자열을 압축해서 표현하고싶습니다.
-```
-입력
-aaabbbbcdddd
-
-출력
-a3b4c1d4
-
-```
 """
 
 collections.Counter('aaabbbbcdddd').items()
