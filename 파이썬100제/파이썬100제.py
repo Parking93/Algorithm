f3a822ecb246d03de8026832e49ce82db7f9da81
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-08-28 23:07:17 +0900

    Update 파이썬100제.py

--- 파이썬100제/파이썬100제.py
+++ 파이썬100제/파이썬100제.py
@@ -302,7 +302,11 @@ format(123456789, ',')
 print(f'{userinput:=^50}')
 
 # 60번
-
+student = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리',
+           '한지호','김진이','김민호','강채연']
+for num, name in enumerate(student):
+    print(f'번호: {num+1}, 이름: {name}')
+    
 # 61번
 
 """
